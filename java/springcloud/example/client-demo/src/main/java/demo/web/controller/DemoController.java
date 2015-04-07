ad9218e6345f68b5124c689fe7f17366ea1b6462
utgard <daniel_sheppard@163.com>
2015-04-07 13:04:53 +0800

    get config from client

--- java/springcloud/example/client-demo/src/main/java/demo/web/controller/DemoController.java
+++ java/springcloud/example/client-demo/src/main/java/demo/web/controller/DemoController.java
@@ -1,5 +1,6 @@
 package demo.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
+	@Value("${foo:xxxx}")
+	String foo;
+
+	@RequestMapping("/")
+	public String index() {
+		return foo;
+	}
 }
