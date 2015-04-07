28cf76dbff883327f050ff70d5bba49f58a8ac85
utgard <daniel_sheppard@163.com>
2015-04-07 11:45:05 +0800

    add @EnableConfigServer demo

--- java/springcloud/example/server-demo/src/main/java/demo/Application.java
+++ java/springcloud/example/server-demo/src/main/java/demo/Application.java
@@ -2,12 +2,14 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
