062f248de4f5f2c4c736c5f4353f5161c386e8a6
utgard <daniel_sheppard@163.com>
2015-04-15 09:25:49 +0800

    impl CommandLineRunner

--- java/springbatch/example/demo/src/main/java/demo/DemoApplication.java
+++ java/springbatch/example/demo/src/main/java/demo/DemoApplication.java
@@ -1,10 +1,41 @@
 package demo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
-public class DemoApplication {
+import demo.batch.Person;
 
-    public static void main(String[] args) {
-		System.exit(SpringApplication.exit(SpringApplication.run(BatchConfiguration.class, args)));
-    }
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchConfiguration.class, args);
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		List<Person> results = jdbcTemplate.query("SELECT first_name, last_name FROM people", new RowMapper<Person>() {
+			@Override
+			public Person mapRow(ResultSet rs, int row) throws SQLException {
+				return new Person(rs.getString(1), rs.getString(2));
+			}
+		});
+
+		for (Person person : results) {
+			System.out.println("Found <" + person + "> in the database.");
+		}
+
+	}
 }
