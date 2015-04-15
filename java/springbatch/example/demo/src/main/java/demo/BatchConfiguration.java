c0761c58fa53e735ae700fae3e6fbc2d4eda761b
utgard <daniel_sheppard@163.com>
2015-04-15 16:29:01 +0800

    add JobParametersValidator

--- java/springbatch/example/demo/src/main/java/demo/BatchConfiguration.java
+++ java/springbatch/example/demo/src/main/java/demo/BatchConfiguration.java
@@ -5,6 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -93,6 +96,12 @@ public class BatchConfiguration {
 				System.out.println("afterJob");
 				System.out.println(jobExecution.toString());
 				System.out.println(jobExecution.getJobParameters());
+			}
+		}).validator(new JobParametersValidator() {
+
+			@Override
+			public void validate(JobParameters parameters) throws JobParametersInvalidException {
+				System.out.println("parameters:" + parameters);
 			}
 		}).incrementer(new RunIdIncrementer()).flow(s1).end().build();
 	}
