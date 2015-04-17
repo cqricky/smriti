992550334f988f2acfd246672c680f6945851d83
utgard <daniel_sheppard@163.com>
2015-04-17 11:50:02 +0800

    run job by jobLauncher

--- java/springbatch/example/demo/src/main/java/demo/batch/BatchService.java
+++ java/springbatch/example/demo/src/main/java/demo/batch/BatchService.java
@@ -1,13 +1,34 @@
 package demo.batch;
 
+import java.util.Date;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BatchService {
 
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
+	JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+
 	@Scheduled(cron = "0/5 * * * * ?")
-	public void test() {
-		System.out.println("xxx");
+	public void test() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		JobExecution result = jobLauncher.run(job, jobParametersBuilder.addDate("time", new Date()).toJobParameters());
+
+		System.out.println(result);
 	}
 }
