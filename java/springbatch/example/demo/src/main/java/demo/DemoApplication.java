71346ec4d97a49f96ea1d9cf21a5112fde9a98ed
utgard <daniel_sheppard@163.com>
2015-04-17 15:09:11 +0800

    配置模块化

--- java/springbatch/example/demo/src/main/java/demo/DemoApplication.java
+++ java/springbatch/example/demo/src/main/java/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan
+@Import({ DataSourceConfig.class, BatchConfiguration.class, JobConfiguration.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchConfiguration.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- java/springbatch/example/demo/src/main/java/demo/batch/BatchService.java
+++ java/springbatch/example/demo/src/main/java/demo/batch/BatchService.java
@@ -4,9 +4,15 @@ import java.util.Date;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -18,18 +24,44 @@ import org.springframework.stereotype.Service;
 public class BatchService {
 
 	@Autowired
-	Job job;
+	JobLauncher jobLauncher;
 
 	@Autowired
-	JobLauncher jobLauncher;
+	JobBuilderFactory jobs;
+
+	@Autowired
+	Step s1;
 
 	JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
 
 	@Scheduled(cron = "0/5 * * * * ?")
 	public void test() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
-		System.out.println("xxxxxxxxx");
+		System.out.println("-----------------------------------------------------------------------------");
+		Job job = jobs.get("importUserJob").listener(new JobExecutionListener() {
+
+			@Override
+			public void beforeJob(JobExecution jobExecution) {
+				System.out.println("beforeJob");
+				System.out.println(jobExecution.toString());
+				System.out.println(jobExecution.getJobParameters());
+			}
+
+			@Override
+			public void afterJob(JobExecution jobExecution) {
+				System.out.println("afterJob");
+				System.out.println(jobExecution.toString());
+				System.out.println(jobExecution.getJobParameters());
+			}
+		}).validator(new JobParametersValidator() {
+
+			@Override
+			public void validate(JobParameters parameters) throws JobParametersInvalidException {
+				System.out.println("parameters:" + parameters);
+			}
+		}).incrementer(new RunIdIncrementer()).flow(s1).end().build();
+
 		JobExecution result = jobLauncher.run(job, jobParametersBuilder.addDate("time", new Date()).toJobParameters());
-		System.out.println("ooooooooo");
 		System.out.println("result:" + result);
+		System.out.println("-----------------------------------------------------------------------------");
 	}
 }
