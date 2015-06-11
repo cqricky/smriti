5016a7d3f9f683eb5ec7837f5359093062c27af9
utgard <daniel_sheppard@163.com>
2015-06-11 11:25:34 +0800

    add os check

--- linux/LFS/script/init.sh
+++ linux/LFS/script/init.sh
@@ -8,7 +8,13 @@ echo "please enter a device..."
 exit 1
 fi
 
+isUbuntu=`cat /etc/issue | grep "Ubuntu" | wc -l`
+
+if [ $isUbuntu == 1 ];then
 . soft-ubuntu.15.04.sh
+else
+. soft-centos.6.6.sh
+fi
 . version-check.sh
 . library-check.sh
 . create-file-system.sh $FORMAT_DEV
