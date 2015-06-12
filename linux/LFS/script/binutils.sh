5a89ca7aa78630a4e1e3cce9ba420f0cd059ce20
utgard <daniel_sheppard@163.com>
2015-06-12 15:09:03 +0800

    update binutils.sh

--- linux/LFS/script/binutils.sh
+++ linux/LFS/script/binutils.sh
@@ -6,6 +6,10 @@ rm -rf ./binutils-build
 mkdir -v binutils-build
 cd binutils-build/
 
+case $(uname -m) in
+x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+
 ../binutils-2.25/configure --prefix=/tools --with-sysroot=$LFS --with-lib-path=/tools/lib --target=$LFS_TGT --disable-nls --disable-werror
 
 cd $LFS/sources
