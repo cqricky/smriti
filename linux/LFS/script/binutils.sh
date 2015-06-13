15efe3ff728df9e57016909ea73eccf4dd047084
utgard <daiel_sheppard@163.com>
2015-06-13 15:45:08 +0800

    delete gcc-dependence.sh

--- linux/LFS/script/binutils.sh
+++ linux/LFS/script/binutils.sh
@@ -10,7 +10,7 @@ case $(uname -m) in
 x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
 esac
 
-../binutils-2.25/configure --prefix=/tools --with-sysroot=$LFS --with-lib-path=/tools/lib --target=$LFS_TGT --disable-nls --disable-werror
+../binutils-2.25/configure --prefix=/tools --with-sysroot=$LFS --with-lib-path=/tools/lib --target=$LFS_TGT --disable-nls --disable-werror && make && make install
 
 cd $LFS/sources
 
